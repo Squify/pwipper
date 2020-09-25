d8a9c2b016ad60e33f3d548bfce08074e7d00d0f
Edwina Tan <edwina.tan@weblib.fr>
2020-09-25 15:50:35 +0200

    resources + migration user categories

---
+++ app/UserCategory.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserCategory extends Model
+{
+    protected $table = "user_categories";
+    
+    protected $fillable = [
+        'name'
+    ];
+
+}
