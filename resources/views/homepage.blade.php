b32efc54ed1d8ee75a7c995d2c9477c4da1b5e28
Pauline <40692004+Squify@users.noreply.github.com>
2020-10-04 22:33:57 +0200

    correct if not connected

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -47,8 +47,8 @@
                     </form>
                 </div>
             </div>
+            <div class="divider-section grey-thin-border"></div>
         @endif
-        <div class="divider-section grey-thin-border"></div>
         <div class="pweep-list">
             @foreach($pweeps as $pweep)
                 <div class="pweep grey-thin-border">
@@ -71,24 +71,28 @@
                                     <small>{{ $pweep->created_at->format('H:m') }}</small>
                                 </p>
                             </div>
-                            <div class="right">
-                                <div class="dropdown">
-                                    <button style="color: white" class="btn" type="button" id="dropdownMenuButton"
-                                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-down"
-                                             fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                            <path fill-rule="evenodd"
-                                                  d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
-                                        </svg>
-                                    </button>
-                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                        <a class="dropdown-item" data-toggle="modal" href="#updatePweep{{$pweep->id}}">Éditer</a>
-                                        <a class="dropdown-item" data-toggle="modal" href="#deletePweep{{$pweep->id}}">Supprimer</a>
+                            @if (Auth::check())
+                                <div class="right">
+                                    <div class="dropdown">
+                                        <button style="color: white" class="btn" type="button" id="dropdownMenuButton"
+                                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-down"
+                                                 fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                                <path fill-rule="evenodd"
+                                                      d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+                                            </svg>
+                                        </button>
+                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                            <a class="dropdown-item" data-toggle="modal"
+                                               href="#updatePweep{{$pweep->id}}">Éditer</a>
+                                            <a class="dropdown-item" data-toggle="modal"
+                                               href="#deletePweep{{$pweep->id}}">Supprimer</a>
+                                        </div>
+                                        @include('components.modal.edit', ['pweep' => $pweep])
+                                        @include('components.modal.remove', ['pweep' => $pweep])
                                     </div>
-                                    @include('components.modal.edit', ['pweep' => $pweep])
-                                    @include('components.modal.remove', ['pweep' => $pweep])
                                 </div>
-                            </div>
+                            @endif
                         </div>
                         <p>{{ $pweep->message}}</p>
                         @if($pweep->image_path_1)
